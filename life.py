e7d0e97fe3c5405a25aabbd28c33b13d99a0221f
Mark Woodbridge <m.woodbridge@imperial.ac.uk>
2018-08-30 12:58:24 +0100

    coverage

--- life.py
+++ life.py
@@ -4,3 +4,9 @@ from scipy import signal
 def count_neighbours(board):
     """Return an array of neighbour counts for each element of `board`"""
     return signal.convolve2d(board, [[1, 1, 1], [1, 0, 1], [1, 1, 1]], mode="same")
+
+
+def step(board):
+    """Return a new board corresponding to one step of the game"""
+    nbrs_count = count_neighbours(board)
+    return (nbrs_count == 3) | (board & (nbrs_count == 2))
--- test_life.py
+++ test_life.py
@@ -1,4 +1,4 @@
-from life import count_neighbours
+from life import count_neighbours, step
 
 
 def test_count_neighbours():
@@ -14,5 +14,22 @@ def test_count_neighbours():
         [1, 2, 3, 2, 1],
         [1, 1, 2, 1, 1],
         [1, 2, 3, 2, 1],
+        [0, 0, 0, 0, 0],
+    ]
+
+
+def test_step():
+    board = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ]
+    assert step(board).tolist() == [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
         [0, 0, 0, 0, 0],
     ]
